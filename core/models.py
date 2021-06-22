14b3686cc1732b89a6bcb309c1181f343108f743
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-06-22 16:40:19 +0200

    add: skill description tests

--- core/models.py
+++ core/models.py
@@ -10,14 +10,29 @@ class Skill(models.Model):
         return self.name['en']
 
     def save(self, *args, **kwargs):
-        error_message = 'Skill object name must be an object with only the "en" and "fr" keys.'
+        error_name = 'Skill object name must be an object with only the "en" and "fr" keys.'
         try:
             if self.name['en'] and self.name['fr']:
-                if len(self.name) == 2:
-                    super(Skill, self).save(*args, **kwargs)
-                else:
-                    raise ValueError(error_message + " Found an extra field.")
+                if len(self.name) != 2:
+                    raise ValueError(error_name + " Found an extra field.")
             else:
-                raise ValueError(error_message)
+                raise ValueError(error_name)
         except KeyError:
-            raise ValueError(error_message)
+            raise ValueError(error_name)
+        except TypeError:
+            raise ValueError(error_name)
+
+        error_description = 'Skill object description must be an object with only the "en" and "fr" keys.'
+        try:
+            if self.description['en'] and self.description['fr']:
+                if len(self.description) != 2:
+                    raise ValueError(error_description +
+                                     " Found an extra field.")
+            else:
+                raise ValueError(error_description)
+        except KeyError:
+            raise ValueError(error_description)
+        except TypeError:
+            raise ValueError(error_description)
+
+        super(Skill, self).save(*args, **kwargs)
--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -63,5 +63,50 @@ class SkillModelTest(TestCase):
                 date=[6, 2021]
             )
 
+    def test_description_non_object(self):
+        """Test passing anything else than an object fails"""
+
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name={"en": "test", "fr": "test"},
+                description=[1, 2, 3],
+                date=[6, 2021]
+            )
+
+    def test_description_missing_en(self):
+        """Test that fails if description.enfield missing"""
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name={"en": "test", "fr": "test"},
+                description={
+                    "fr": "J'ai appris à utiliser MDX"
+                },
+                date=[6, 2021]
+            )
+
+    def test_description_missing_fr(self):
+        """Test that fails if description.fr field missing"""
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name={"en": "test", "fr": "test"},
+                description={
+                    "en": "J'ai appris à utiliser MDX"
+                },
+                date=[6, 2021]
+            )
+
+    def test_description_extra_field(self):
+        """Test that fails if extra field other than 'fr' or 'en'"""
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name={"en": "test", "fr": "test"},
+                description={
+                    "en": "I learned to use MDX",
+                    "fr": "J'ai appris à utiliser MDX",
+                    "extra": "wrong"
+                },
+                date=[6, 2021]
+            )
+
     def test_skill_str(self):
         self.assertEqual(str(self.skill), self.skill.name['en'])
