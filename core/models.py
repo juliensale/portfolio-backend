4177b80aa7771d0e10483571d7b029b2540bb423
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-06-22 16:32:29 +0200

    add: skill model & name field tests

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.JSONField()
+    description = models.JSONField()
+    date = models.JSONField()
+
+    def __str__(self):
+        return self.name['en']
+
+    def save(self, *args, **kwargs):
+        error_message = 'Skill object name must be an object with only the "en" and "fr" keys.'
+        try:
+            if self.name['en'] and self.name['fr']:
+                if len(self.name) == 2:
+                    super(Skill, self).save(*args, **kwargs)
+                else:
+                    raise ValueError(error_message + " Found an extra field.")
+            else:
+                raise ValueError(error_message)
+        except KeyError:
+            raise ValueError(error_message)
---
+++ core/tests/test_models.py
@@ -0,0 +1,67 @@
+from django.test import TestCase
+from core import models
+
+
+class SkillModelTest(TestCase):
+
+    def setUp(self):
+        self.skill = models.Skill.objects.create(
+            name={'en': "MDX", 'fr': "MDX"},
+            description={
+                "en": "I learned to use MDX",
+                "fr": "J'ai appris à utiliser MDX"
+            },
+            date=[6, 2021]
+        )
+
+    def test_name_non_object(self):
+        """Test passing anything else than an object fails"""
+
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name=[1, 2, 3],
+                description={
+                    "en": "I learned to use MDX",
+                    "fr": "J'ai appris à utiliser MDX"
+                },
+                date=[6, 2021]
+            )
+
+    def test_name_missing_en(self):
+        """Test that fails if name.enfield missing"""
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name={"fr": "test"},
+                description={
+                    "en": "I learned to use MDX",
+                    "fr": "J'ai appris à utiliser MDX"
+                },
+                date=[6, 2021]
+            )
+
+    def test_name_missing_fr(self):
+        """Test that fails if name.fr field missing"""
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name={"en": "test"},
+                description={
+                    "en": "I learned to use MDX",
+                    "fr": "J'ai appris à utiliser MDX"
+                },
+                date=[6, 2021]
+            )
+
+    def test_name_extra_field(self):
+        """Test that fails if extra field other than 'fr' or 'en'"""
+        with self.assertRaises(ValueError):
+            models.Skill.objects.create(
+                name={"en": "test", "fr": "test", "extra": "wrong"},
+                description={
+                    "en": "I learned to use MDX",
+                    "fr": "J'ai appris à utiliser MDX"
+                },
+                date=[6, 2021]
+            )
+
+    def test_skill_str(self):
+        self.assertEqual(str(self.skill), self.skill.name['en'])
