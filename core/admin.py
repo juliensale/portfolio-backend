f6ccee3c8d2a5b58105b955bb08c4406007ed385
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-07-13 08:54:06 +0200

    add: image urls

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from core.models import Review
+from core.models import Review, Technology
 
 
 class ReviewAdmin(admin.ModelAdmin):
     pass
 
 
+class TechnologyAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(Technology, TechnologyAdmin)
