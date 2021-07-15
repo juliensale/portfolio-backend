c18c1daac3ba9bd2f02fd525ef2a7050dcf9510c
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-07-15 07:15:31 +0200

    clean: removed uploads path

--- backend/settings.py
+++ backend/settings.py
@@ -126,9 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/uploads/'
-
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'backend/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
